3ecce09017c262d984705dbeb0643b6ae39083fb
Rio <rjmssevilla@gmail.com>
2021-02-27 06:57:17 +0400

    fixed migration file

--- db/migrate/20210221215115_change_data_type_for_deadline.rb
+++ db/migrate/20210221215115_change_data_type_for_deadline.rb
@@ -1,9 +1,11 @@
 class ChangeDataTypeForDeadline < ActiveRecord::Migration[6.1]
   def up
-    change_column :tasks, :deadline, :datetime
+    remove_column :tasks, :deadline
+    add_column :tasks, :deadline, :datetime
   end
     
   def down
-    change_column :tasks, :deadline, :string
+    remove_column :tasks, :deadline
+    add_column :tasks, :deadline, :string
   end
 end
